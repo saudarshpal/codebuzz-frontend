17bf338c4bbb3c763297503a5722a496dac11004
Saudarsh Pal <saudarsh29@gmail.com>
2026-05-17 20:27:31 +0530

    feat: setup react query client and global providers warapping
    - create a dedicated providers.tsx for root wrapping
    - initialize QueryClient, QueryClientProvider and ReactQueryDevtools

---
+++ app/providers.tsx
@@ -0,0 +1,17 @@
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactNode } from "react";
+import { queryClient } from "./lib/queryClient";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+
+const Providers = ({ children} : { children : ReactNode}) => {
+    return(
+        <QueryClientProvider client={queryClient}>
+            {children}
+            <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+    )
+}
+
+
+export default Providers
